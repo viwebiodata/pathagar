0c3db046ee5d4f93ee8cc7da4e14489b05424e8a
viwebiodata <sabbirahammeddata@gmail.com>
2026-07-19 21:39:51 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Apps Script ডিপ্লয় করার পর যে Web App URL পাবেন সেটা এখানে বসান
 // উদাহরণ: https://script.google.com/macros/s/AKfycb.../exec
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbxX0GmK5DNqxOS31iN_TdBidx2r83l5O3wB0aHYhfsID127gW4VXZrXz9Esx6yy6JX7/exec";
